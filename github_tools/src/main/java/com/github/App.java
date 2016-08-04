bf412112c3c44825c189d686c7804e7d9e8acc43
Willy Aguirre <marti1125@gmail.com>
2016-08-04 12:04:45 -0500

    init project

---
+++ github_tools/src/main/java/com/github/App.java
@@ -0,0 +1,18 @@
+package com.github;
+
+import org.jooby.Jooby;
+
+/**
+ * @author jooby generator
+ */
+public class App extends Jooby {
+
+  {
+    get("/", () -> "Hello World!");
+  }
+
+  public static void main(final String[] args) throws Throwable {
+    run(App::new, args);
+  }
+
+}
